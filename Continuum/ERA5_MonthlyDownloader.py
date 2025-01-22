bc38dd2dd25adeb1f4963b5f82cd00c694e92733
lwalls2019 <liz.walls99@gmail.com>
2025-01-22 09:50:01 -0800

    Fix to ERA5 downloader

--- Continuum/ERA5_MonthlyDownloader.py
+++ Continuum/ERA5_MonthlyDownloader.py
@@ -41,6 +41,8 @@ elif incl10mWS and not incl10mGust:
     varStr = ["100m_u_component_of_wind", "100m_v_component_of_wind", "2m_temperature", "surface_pressure", "10m_u_component_of_wind", "10m_v_component_of_wind"]
 elif incl10mWS and incl10mGust:
     varStr = ["100m_u_component_of_wind", "100m_v_component_of_wind", "2m_temperature", "surface_pressure", "10m_u_component_of_wind", "10m_v_component_of_wind", "10m_wind_gust_since_previous_post_processing"]
+elif not incl10mWS and incl10mGust:
+    varStr = ["100m_u_component_of_wind", "100m_v_component_of_wind", "2m_temperature", "surface_pressure", "10m_wind_gust_since_previous_post_processing"]
 
 
 #output_file = 'ERA5_N' + str(min_lat) + "_to_" + str(max_lat) + "_W" + str(min_lon) + '_to_' + str(max_lat) + '_' + str(req_year)  + str(req_month) + str(req_day) + '.nc'
